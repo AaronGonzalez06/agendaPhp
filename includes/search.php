281c004c8d8bd3f0120874efa98af451b584207a
AaronGonzalez06 <aaron060812060812@gmail.com>
2023-07-11 11:54:32 +0200

    Nuevos estilos

--- includes/search.php
+++ includes/search.php
@@ -1,4 +1,4 @@
-<form method="POST" action="search.php"  class="form-inline my-2 my-lg-0">
+<form method="POST" action="search.php"  class="form-inline my-2 my-lg-0 d-flex formMod">
                         <input name="fecha" class="form-control mr-sm-2" type="search" placeholder="Dia a buscar..." aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                      </form>
+                        <button class="btn btn-info my-2 my-sm-0 btnMod" type="submit">Search</button>
+</form>
